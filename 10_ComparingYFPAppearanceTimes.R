4f06aca14d0cb240a7bc001629a741cc1bca24f4
thomasyounghi <thomaszhyoung@gmail.com>
2021-02-28 00:59:56 -1000

    corrected file path for functions, cleaned up comments

--- 10_ComparingYFPAppearanceTimes.R
+++ 10_ComparingYFPAppearanceTimes.R
@@ -1,16 +1,22 @@
-#Her we compare the time taken for YFP to appear
-setwd('/Users/thomasyoung/Dropbox/MovieProcessing/March2018_Analysis/')
-source('/Users/thomasyoung/Dropbox/templates/R_aging_template/functions/Preprocessing_func.Rd')
-source('/Users/thomasyoung/Dropbox/templates/R_aging_template/functions/timeseries_func.Rd')
-source('/Users/thomasyoung/Dropbox/templates/R_aging_template/functions/func.Rd')
+#Comparing the time taken for YFP to appear across all experiments.
+#The mean and sem for replicate averaged YFP appearance times are plotted.
+#Results are saved in './figures/YFPappearancetimes/'
+
+setwd('/Users/thomasyoung/Dropbox/MovieProcessing/March2018_Analysis_git')
+source('./functions/timeseries_func.Rd')
+source('./functions/func.Rd')
+source('./functions/Preprocessing_func.Rd')
+
+library(ggplot2)
 library(dplyr)
 library(cowplot)
 library(reshape2)
-library(plotmath)
+#library(plotmath)
 
 
 #original ebwidth is 0.25
 #figure settings:
+theme_set(theme_cowplot())
 themes = theme(axis.text=element_text(size=18), axis.title=element_text(size=16),strip.text.x = element_text(size = 16))
 ylims = ylim(-0,25)
 jitterw = 0.25
@@ -30,11 +36,8 @@ strain_labeller <- function(variable,value){
 }
 
 
-
-
 #folder to save in
 outfolder = './figures/YFPappearancetimes/'
-
 
 
 info = read.csv('./CombinedData/info_offatdox_alive5hafter.csv')
@@ -42,10 +45,9 @@ info = filter(info,expstrain!='SSAcontrol' & expstrain!='SSAdegcontrol' & expstr
 info$expstrain2 = factor(info$expstrain,levels = c('SSAcontrol','SSA','SSA Dnl4ko','SSAdeg','SSAdegcontrol','SSA3xCln2','SSAhet'),labels = c("SSAcontrol",'SSA',expression(paste('SSA ',italic('dnl4'),Delta)),'SSA+RFPdegron','SSAcontrol+RFPdegron','SSA3xCln2',"SSAheterology"))
 info$expage = factor(info$expage,levels=c('young','old'))
 
+
 #Among the cells in which YFP appears, is there any difference in when it appears between young and old cells
-#If it takes longer to appear in old cells, that would suggest that delay in cutting, repair or YFP expression could be one #reason why a lower % of cells is detected as repair.  If it is similar to between old and young, it means that at in cells in #which repair occurs, repair, expression, etc is fast
-
-
+#If it takes longer to appear in old cells, that would suggest that delay in cutting, repair or YFP expression could be a reason why a lower % of cells are detected as repaired.
 condition = info$yfpclass == 'turnedon';
 info = filter(info,condition)
 firstontimeh = info$lastofftime/2;
@@ -64,9 +66,7 @@ removeggplottext(p1,plotfilename,4,600,9,4.5)
 
 
 
-p1 = ggplot(info,aes(expage,lastobservationtime))+geom_boxplot(width=ebwidth)+geom_jitter(height=0,width = jitterw,aes(colour=factor(replabel)))+facet_grid(.~expstrain2,labeller=label_parsed)+geom_hline(aes(yintercept=doxtime+14*6))
-
-ylim(0,40) + ylabel + xlab('age') + geom_hline(yintercept=140/60) + geom_point(data=meanandsem,aes(expage,firstontimeh))+labs(colour = 'Burst?') + themes + border
+p1 = ggplot(info,aes(expage,lastobservationtime))+geom_boxplot(width=ebwidth)+geom_jitter(height=0,width = jitterw,aes(colour=factor(replabel)))+facet_grid(.~expstrain2,labeller=label_parsed)+geom_hline(aes(yintercept=doxtime+14*6)) + ylabel + xlab('age') + geom_hline(yintercept=140/60) + geom_point(data=meanandsem,aes(expage,firstontimeh))+labs(colour = 'Burst?') + themes + border
 plotfilename = paste(outfolder,'lastobservationtimes_all.pdf',sep="") 
 ggsave(file = plotfilename,p1,dpi=600,width=9,height=4.5)
 removeggplottext(p1,plotfilename,4,600,9,4.5)

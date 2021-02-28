31f39903f5925e8656c73b239ac74fdde550fb1d
thomasyounghi <thomaszhyoung@gmail.com>
2021-02-27 21:52:03 -1000

    corrected file path for functions, added more descriptive comments

--- 5_brightneighboringcells.R
+++ 5_brightneighboringcells.R
@@ -1,12 +1,17 @@
-#This script identifies problematic neighbor measurements based on the files in './NeighborMeasurements'
-#In the folder './ProblematicNeighborMeasurements/' outputs a 2 lists containing problematic yfp and rfp measurements: 'problematicyfps.csv' and 'problematicrfps.csv'.  These list the time, and xy location, and trap for problematic measurements. If it was possible to measure a test region nearby the bright neighbor for background, the mean of these test regions is calculated
-#Neighbor measurements are considered problematic if their mean pixel intensity is > 200 AU above the average pixel intensity measured over the entire local rectangle.
+#This script identifies problematic neighbor cell measurements based on the files in './NeighborMeasurements'
+#Neighbor measurements are considered problematic if their mean pixel intensity is > 200 AU above the average pixel intensity measured over the entire local rectangle. The brightness of these neighbors adds to the signal measured for the cell of interest and is not accounted for by background subtraction
+
+
+#Outputs 2 lists with the problematic measurements in the folder './ProblematicNeighborMeasurements/': 'problematicyfps.csv' and 'problematicrfps.csv' 
+#The lists contain the time, and xy location, and trap number for problematic measurements. 
+
 
 #we need to specify how the columns of the two 
-setwd('/Users/thomasyoung/Dropbox/MovieProcessing/March2018_Analysis/')
-source('/Users/thomasyoung/Dropbox/templates/R_aging_template/functions/Preprocessing_func.Rd')
-source('/Users/thomasyoung/Dropbox/templates/R_aging_template/functions/timeseries_func.Rd')
-source('/Users/thomasyoung/Dropbox/templates/R_aging_template/functions/func.Rd')
+setwd('/Users/thomasyoung/Dropbox/MovieProcessing/March2018_Analysis_git')
+source('./functions/timeseries_func.Rd')
+source('./functions/func.Rd')
+source('./functions/Preprocessing_func.Rd')
+
 library(dplyr)
 library(ggplot2)
 library(reshape2)
@@ -31,30 +36,26 @@ localbg$date = localbgdates
 neighbors$date = neighbordates
 
 
-
 #Take average of local test bgs. Obtain counts as well
 bgtestroistats = localbg %>% group_by(date,xy,trap,time,neighborid) %>% summarize(avgtestyfp = mean(yfpmean),avgtestrfp = mean(rfpmean),sdtestyfp = sd(yfpmean),sdtestrfp = sd(rfpmean),testcount = n())
 
 allneighborstats = left_join(neighbors,bgtestroistats,by=c("date","xy","trap","time","neighborid"))
 
 
-#Read in the manualbgmeasurements (averaged over a larger area)
-#We want to compare these with the brightness of the neighboring cells to determine whether #The local fluorescence will have 
+#Read in the manualbgmeasurements (averaged over a larger rectangular area)
+#left join to the neighboring cell measurements at the same location and time
 rawbg = read.csv('./CombinedData/cleanedconcatrawbg.csv')
 allneighborstats = left_join(allneighborstats,rawbg,by=c("date","xy","trap","time"))
 
 
+#Background subtracting the neighboring cell measurements
 allneighborstats = mutate(allneighborstats,yfpabovebg = yfpmean-c2avgbgpix,rfpabovebg = rfpmean-c3avgbgpix)
 allneighborstats = allneighborstats[!is.na(allneighborstats$numbgpix),]
 
 
-#We will only concern ourselves with the neighbor if it is very bright > 200 above the bg fluroescence.
+
+#Neighboring cells are problematic if their average fluorescence is > 200 above the bg fluroescence and they are a distance of 15 pixels or less from the center of the cell of interest.
 #A measurement that is problematic in yfp might not be problematic in rfp and vice versa.
-#We define a rule to call a bright neighbor problematic (>200 above the empirically measured bg, and a distance of 15 or less from the center of the roi).
-
-
-#Even if a neighbor is problematic and there is no test fluorescence measurement, it doesn't mean that we can't still make a decent evaluation of the cell of interest.  If the cell of interest has far higher fluoresence than the neighbor, then it is probably positive for the fluorescence.
-
 brightyfpneighbor = (allneighborstats$yfpabovebg>200) & (allneighborstats$disttotrapcenter<15)
 brightrfpneighbor = (allneighborstats$rfpabovebg>200) & (allneighborstats$disttotrapcenter<15)
 
@@ -62,7 +63,8 @@ problematicyfps = allneighborstats[brightyfpneighbor,]
 problematicrfps = allneighborstats[brightrfpneighbor,]
 
 
-#If the trapped cell of interest is just as bright, its not that big a deal, If the trapped cell of interest is middling in brightness, that could be due to the bright neighbor.
+
+#Saving the problematic neighbor measurements
 write.csv(problematicyfps,'./ProblematicNeighborMeasurements/problematicyfps.csv',row.names=FALSE)
 write.csv(problematicrfps,'./ProblematicNeighborMeasurements/problematicrfps.csv',row.names=FALSE)
 
